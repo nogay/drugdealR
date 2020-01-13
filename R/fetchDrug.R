a8e4b935aa773156b4ca4c2fdaabd2e27173db40
Nogayhan Seymen <nogayhan.seymen@live.acibadem.edu.tr>
2020-01-13 11:14:13 +0300

    first function

--- R/fetchDrug.R
+++ R/fetchDrug.R
@@ -1,5 +1,15 @@
-library(XML)
-xmldoc <- xmlParse("./data/drugbank.xml")
-rootNode <- xmlRoot(xmldoc)
-data <- xmlSApply(rootNode,function(x) xmlSApply(x, xmlValue))
-cd.catalog <- data.frame(t(data),row.names=NULL)
+#' Load Drugbank data
+#'
+#' @param x character
+#'
+#' @return A dataframe
+#' @export
+#'
+#' @examples
+#' fetchDrug("./data/drugbank.xml")
+fetchDrugbank <- function(x){
+  xmldoc <- xmlParse(x)
+  rootNode <- xmlRoot(xmldoc)
+  data <- xmlSApply(rootNode,function(x) xmlSApply(x, xmlValue))
+  cd.catalog <- data.frame(t(data),row.names=NULL)
+}
