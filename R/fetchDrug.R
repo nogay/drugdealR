a8aeec1cad74874dfda7f7f33fa0ec03c2386a29
Nogayhan Seymen <nogayhan.seymen@live.acibadem.edu.tr>
2020-02-11 16:37:02 +0300

    le commit

--- R/fetchDrug.R
+++ R/fetchDrug.R
@@ -11,9 +11,19 @@ fetchDrugbank <- function(){
   colnames(DRUG_DF) <- c("name", "approval", "genes")
   DRUG_DF$genes <- gsub("\\{", "", DRUG_DF$genes)
   DRUG_DF$genes <- gsub("\\}", "", DRUG_DF$genes)
+  DRUG_DF$genes <- gsub("\\'", "", DRUG_DF$genes)
 
-  DRUG_DF <- DRUG_DF %>%
-    separate('genes', paste0('genes', c(1:(max(str_count(DRUG_DF$genes, ","))+1))), sep = ',', remove = T)
+  DRUG_DF <- separate(DRUG_DF, 'genes', paste0('genes', c(1:(max(str_count(DRUG_DF$genes, ","))+1))), sep = ',', remove = T)
+
+  DRUG_DF <- pivot_longer(DRUG_DF,
+    cols = starts_with("genes"),
+    names_to = "genes",
+    values_drop_na = TRUE
+  )
+  DRUG_DF <- DRUG_DF[DRUG_DF$value != "",]
+  DRUG_DF <- DRUG_DF[,c("name","approval","value")]
+
+  DRUG_DF <- separate(DRUG_DF, "value", c("gene","action"), sep=":")
 
   return(DRUG_DF)
 }
--- R/fetchSignor.R
+++ R/fetchSignor.R
@@ -1,3 +1,10 @@
+#' Load SIGNOR 2.0 data (Downloaded February 2020)
+#'
+#' @return dataframe
+#' @export
+#'
+#' @examples
+#' fetchSignor()
 fetchSignor <- function(){
   SIGNOR_DF <- read.csv("./data/signor_11022020.tsv", sep="\t")
   SIGNOR_DF <- SIGNOR_DF[,c("ENTITYA","TYPEA","IDA","ENTITYB","TYPEB","IDB","EFFECT")]
